123629a6dbdcdf0ce3ac59e8eb6fdb620b759804
Juan Romero Francia <juancarlos.romero@diveria.com>
2025-12-22 18:02:51 -0300

    Eliminar configuración experimental de Turbo en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        '@': './app',
-      },
-    },
-  },
 };
 
 export default nextConfig;
